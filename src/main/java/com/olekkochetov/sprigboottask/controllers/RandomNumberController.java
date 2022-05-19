b8cff06604e0f3b971f6fba08ff8cf0c8d852787
alexkochetov <kchetovalex@yahoo.com>
2022-05-19 09:50:51 +0200

    Add controller

---
+++ src/main/java/com/olekkochetov/sprigboottask/controllers/RandomNumberController.java
@@ -0,0 +1,25 @@
+package com.olekkochetov.sprigboottask.controllers;
+
+import java.util.HashMap;
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RandomNumberController {
+	private HashMap<Integer, Integer> hashNums = new HashMap<>();
+	private Random rand = new Random();
+	
+	@GetMapping("/")
+	public int getRandomNumbers(@RequestParam(value="id", defaultValue= "1") int id) {
+			if(!hashNums.containsKey(id)) {
+				hashNums.put(id, rand.nextInt());
+				return hashNums.get(id);
+			}
+			else {
+				return hashNums.get(id);
+			}
+	}
+}
