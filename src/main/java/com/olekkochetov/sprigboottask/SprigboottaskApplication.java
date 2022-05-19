7c01ede584eff5098de0f5491f1a4f7af7708d5e
alexkochetov <kchetovalex@yahoo.com>
2022-05-19 09:45:10 +0200

    Initial commit

---
+++ src/main/java/com/olekkochetov/sprigboottask/SprigboottaskApplication.java
@@ -0,0 +1,13 @@
+package com.olekkochetov.sprigboottask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigboottaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprigboottaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olekkochetov/sprigboottask/SprigboottaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olekkochetov.sprigboottask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigboottaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
